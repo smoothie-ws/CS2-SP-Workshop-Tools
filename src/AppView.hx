7026c866be0b140a6198aa546ade2aa07357879c
smoothie <kibot.email@gmail.com>
2024-02-04 21:38:56 +0800

    App tweaks

--- src/AppView.hx
+++ src/AppView.hx
@@ -1,10 +1,43 @@
 package;
 
 import haxe.ui.containers.VBox;
+import openfl.events.MouseEvent;
+import openfl.Lib;
 
-@:build(haxe.ui.ComponentBuilder.build("assets/main-view.xml"))
+@:build(haxe.ui.ComponentBuilder.build("assets/app-view.xml"))
 class AppView extends VBox {
+	private var oldPos:openfl.geom.Point;
+
 	public function new() {
 		super();
+		this.addEventListener(MouseEvent.MOUSE_DOWN, onMouseDown);
+		this.addEventListener(MouseEvent.MOUSE_UP, onMouseUp);
+		this.addEventListener(MouseEvent.MOUSE_MOVE, onMouseMove);
+	}
+
+	private function onMouseDown(event:MouseEvent):Void {
+		if (event.buttonDown) {
+			oldPos = new openfl.geom.Point(event.stageX, event.stageY);
+		}
+	}
+
+	private function onMouseUp(event:MouseEvent):Void {
+		if (!event.buttonDown) {
+			oldPos = null;
+		}
+	}
+
+	private function onMouseMove(event:MouseEvent):Void {
+		try {
+			if (oldPos == null) {
+				return;
+			}
+			var delta:openfl.geom.Point = new openfl.geom.Point(event.stageX, event.stageY).subtract(oldPos);
+			Lib.current.stage.window.x += Std.int(delta.x);
+			Lib.current.stage.window.y += Std.int(delta.y);
+			oldPos = new openfl.geom.Point(event.stageX, event.stageY);
+		} catch (error:Dynamic) {
+			trace(error);
+		}
 	}
 }
